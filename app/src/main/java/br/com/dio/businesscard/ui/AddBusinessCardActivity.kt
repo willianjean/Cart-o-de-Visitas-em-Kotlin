89cf0f7fceb7ef7a1d41c34a542e25b6cf4dc34f
Willian Jean <willianjean@gmail.com>
2021-08-12 08:35:17 -0400

    CardView e RecyclerView xml implementado

---
+++ app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
@@ -0,0 +1,25 @@
+package br.com.dio.businesscard.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.dio.businesscard.databinding.ActivityAddBusinessCardBinding
+
+class AddBusinessCardActivity : AppCompatActivity() {
+
+    private val binding by lazy {ActivityAddBusinessCardBinding.inflate(layoutInflater)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        insertListener()
+    }
+
+    private fun insertListener(){
+        binding.btnClose.setOnClickListener{
+            finish()
+        }
+        binding.btnConfirm.setOnClickListener{
+
+        }
+    }
+}
